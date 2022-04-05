53f8e95a3ce7dd439a82ffe94e4c2fa5a0c291be
Nannaleigh <tatianna.martinez71@gmail.com>
2022-04-04 19:49:19 -0600

    Completed Tasks, Task, Add Task

---
+++ src/Components/Tasks.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import Task from "./Task";
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <div>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
+      ))}
+    </div>
+  );
+};
+
+export default Tasks;
